5049a80e7ba0f6f3cecb11dbddebc73e06dafd36
Marlon Eloy <112662225+MarlonBES@users.noreply.github.com>
2025-09-09 00:27:40 +0000

    projeto e atividade 2.B POO

--- Program.cs
+++ Program.cs
@@ -1,9 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
- 
+﻿Console.WriteLine("Hello, World!");
+
 Televisao tv = new Televisao(55f);
 
-Console.WriteLine($"A TV tem o tamanho {tv.Tamanho}");
+Console.WriteLine($"A TV tem o tamanho {tv.Tamanho} polegadas");
+
+tv.Ligar();
 
 tv.AumentarVolume();
-Console.WriteLine($"Volume(tv.Volume)");
+Console.WriteLine($"Volume: {tv.Volume}");
+
+tv.DiminuirVolume();
+Console.WriteLine($"Volume: {tv.Volume}");
+
+tv.AtivarMudo();
+tv.DesativarMudo();
+
+tv.DefinirCanal(100);
+Console.WriteLine($"Canal atual: {tv.Canal}");
+
+tv.ProximoCanal();
+Console.WriteLine($"Canal atual: {tv.Canal}");
+
+tv.CanalAnterior();
+Console.WriteLine($"Canal atual: {tv.Canal}");
+
+tv.Desligar();
+
--- televisão.cs
+++ televisão.cs
@@ -1,22 +1,84 @@
-public class Televisao{
+public class Televisao
+{
+    private const int VOL_MAX = 100;
+    private const int VOL_MIN = 0;
+    private const int CANAL_MAX = 520;
+    private const int CANAL_MIN = 1;
 
-    private int const int VOL_MAX = 100;
     public Televisao(float tamanho)
     {
         Tamanho = tamanho;
+        Canal = 1;
+        Volume = 10;
     }
 
     public float Tamanho { get; set; }
     public int Resolucao { get; set; }
-    public int Volume {get; private set; }
-    public int Canal {get; set; }
-    public bool Estado {get; set;}
-    
+    public int Volume { get; private set; }
+    public int Canal { get; private set; }
+    public bool Estado { get; private set; }
+    private int ultimoCanalAssistido;
+    private bool mudo;
+
     public void AumentarVolume()
     {
-        if(Volume < 100)
-        Volume = Volume + 1;
+        if (Volume < VOL_MAX)
+            Volume++;
+    }
+
+    public void DiminuirVolume()
+    {
+        if (Volume > VOL_MIN)
+            Volume--;
+    }
+
+    public void AtivarMudo()
+    {
+        mudo = true;
+        Console.WriteLine("Mudo ativado.");
+    }
+
+    public void DesativarMudo()
+    {
+        mudo = false;
+        Console.WriteLine("Mudo desativado.");
+    }
+
+    public void DefinirCanal(int novoCanal)
+    {
+        if (novoCanal >= CANAL_MIN && novoCanal <= CANAL_MAX)
+        {
+            Canal = novoCanal;
+        }
         else
-        Console.WriteLine("Tv já está no max.")
+        {
+            Console.WriteLine("Canal inválido.");
+        }
+    }
+
+    public void ProximoCanal()
+    {
+        if (Canal < CANAL_MAX)
+            Canal++;
+    }
+
+    public void CanalAnterior()
+    {
+        if (Canal > CANAL_MIN)
+            Canal--;
+    }
+
+    public void Ligar()
+    {
+        Estado = true;
+        Canal = ultimoCanalAssistido == 0 ? 1 : ultimoCanalAssistido;
+        Console.WriteLine($"TV ligada no canal {Canal}");
+    }
+
+    public void Desligar()
+    {
+        Estado = false;
+        ultimoCanalAssistido = Canal;
+        Console.WriteLine("TV desligada.");
     }
 }
